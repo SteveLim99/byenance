4b7bd5a136646536c2d983bdfe935daa768bed7d
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-05-23 20:59:05 +0800

    fd global

--- apps/api/app.py
+++ apps/api/app.py
@@ -3,16 +3,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from fetch_data import FetchData
 
 app = Flask(__name__)
+fd = FetchData()
 
 
 @app.before_first_request
 def update_db():
-    fd = FetchData()
     fd.run()
 
 
 def hourly_db_update():
-    fd = FetchData()
     fd.run()
 
 
