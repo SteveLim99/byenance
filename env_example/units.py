c56ed049edcbf961bb74e17d3214bbb7591ec387
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-05-24 21:55:10 +0800

    added env example

---
+++ env_example/units.py
@@ -0,0 +1,5 @@
+import datetime as datetime
+
+SCRAP_UNITS = {"BTCUSDT", "ETHUSDT"}
+# FORMAT datetime(<YEAR>, <MONTH>, <DATE>, <HOUR>, <MINUTE>)
+DEFAULT_START_DATE = datetime.datetime(2018, 1, 1, 0, 0)
